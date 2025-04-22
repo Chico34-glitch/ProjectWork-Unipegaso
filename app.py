1fc7cc4a77a2c3c1333fb42aba17454a368fe982
Chico34-glitch <christiancostareal@gmail.com>
2025-04-22 23:32:49 +0200

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from flask_cors import CORS
 import sqlite3
 
-app = Flask(_name_)
+app = Flask(__name__)
 CORS(app)
 
 app.config['SECRET_KEY'] = 'super-secret'
